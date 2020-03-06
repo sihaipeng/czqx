c2f899f017b63a25bc5a5974657328b3220cc877
sihaipeng <sihaipeng52@gmail.com>
2020-03-06 15:54:08 +0800

    基础功能

---
+++ src/test/java/com/czqx/ordinary/OrdinaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czqx.ordinary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdinaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
